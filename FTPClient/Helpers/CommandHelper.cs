b92d76e9a93c8ad8f5862e66e3f46ec31e90650b
Sudocat <i@sudocat.me>
2020-07-02 11:14:04 +0800

    AddressParser

--- FTPClient/Helpers/CommandHelper.cs
+++ FTPClient/Helpers/CommandHelper.cs
@@ -5,9 +5,29 @@ namespace FTPClient.Helpers
 {
     public class CommandHelper
     {
-        static IPEndPoint AddressParser(string s)
+        public static IPEndPoint AddressParser(string s)
         {
-            throw new NotImplementedException();
+            var str = GetMiddleText(s, "(", ")");
+            var IPE = str.Split(',');
+            if (IPE.Length != 6)
+                throw new NotImplementedException();
+            var ip = IPAddress.Parse(string.Format("{0}.{1}.{2}.{3}", IPE[0], IPE[1], IPE[2], IPE[3]));
+            var port = Convert.ToInt32(IPE[4]) * 256 + Convert.ToInt32(IPE[5]);
+            return new IPEndPoint(ip, port);
+        }
+
+        private static string GetMiddleText(string t, string k, string j) //取出中间文本
+        {
+            try
+            {
+                var kn = t.IndexOf(k, StringComparison.Ordinal) + k.Length;
+                var jn = t.IndexOf(j, kn, StringComparison.Ordinal);
+                return t.Substring(kn, jn - kn);
+            }
+            catch
+            {
+                return "";
+            }
         }
     }
 }
