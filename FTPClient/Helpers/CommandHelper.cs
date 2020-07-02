7a0fbcd17b8b3d256a8538f0216d0a01fd8358c3
Eric_Lian <git@superexercisebook.com>
2020-07-02 10:35:38 +0800

    Socket Client | Helper | IPE Parser

---
+++ FTPClient/Helpers/CommandHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace FTPClient.Helpers
+{
+    public class CommandHelper
+    {
+        static IPEndPoint AddressParser(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
